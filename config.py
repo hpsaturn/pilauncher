f41e85eaf25a582a1894db4a94d941beaff9688e
Hpsaturn <hpsaturn@gmail.com>
2023-10-13 22:10:40 +0200

    abstraction object of the config YML file

---
+++ config.py
@@ -0,0 +1,57 @@
+import yaml
+
+class app:
+    def __init__(self,name):
+        self.name = name
+        self.cmds = []
+
+    def get_name(self):
+        return self.name
+    
+    def add_cmd(self, cmd):
+        self.cmds.append(cmd)
+    
+    def __str__(self):
+        out = self.name+':'
+        for cmd in self.cmds:
+            out=out+'\r\n'+cmd.name+' -> '+cmd.command
+        return f"{out}"
+    
+
+class config:
+    def __init__(self,config):
+        self.apps = []
+        with open(config, 'r') as file:
+            docs = yaml.safe_load_all(file)
+            for doc in docs:
+                self.laod_app(doc)
+
+    def laod_app(self, doc):
+        for name in dict.fromkeys(doc):
+            # print(name)
+            ap = app(name)
+            for cmds in dict.fromkeys(doc[name]):
+                # print(' '+cmds+' -> '+doc[name][cmds]['cmd'])
+                cmd = app(cmds)
+                cmd.command = doc[name][cmds]['cmd']
+                ap.add_cmd(cmd)
+            
+            self.apps.append(ap)
+    
+    def __str__(self):
+        out = ''
+        for app in self.apps:
+            out=out+str(app)
+        return f"{out}"
+
+
+
+# ac = config('config.yml')
+
+# print(ac)
+# print(ac.apps[0])
+# print(ac.apps[1])
+# print(ac.apps[0].cmds[0].name)
+# print(ac.apps[0].cmds[1].name)
+# print(ac.apps[1].cmds[0].name)
+# print(ac.apps[1].cmds[1].name)
