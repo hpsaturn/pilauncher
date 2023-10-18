d504c7a88570bda8dec83a6b188eb51557db35bb
Hpsaturn <hpsaturn@gmail.com>
2023-10-18 23:49:13 +0200

    improved app status line redraw

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import time
 import subprocess
 import threading
-
-from board import SCL, SDA
-import busio
-from PIL import Image, ImageDraw, ImageFont
+import board
+import digitalio
 import adafruit_ssd1306
+from PIL import Image, ImageDraw, ImageFont
 from gui import GuiManager
+
+WIDTH = 128
+HEIGHT = 32  # Change to 64 if needed
+BORDER = 1
 
 import RPi.GPIO as GPIO
 BTNLFT = 23
@@ -21,12 +24,10 @@ onStatusTask = False
 gm = GuiManager()
 
 # Create the I2C interface.
-i2c = busio.I2C(SCL, SDA)
-
-# Create the SSD1306 OLED class.
-# The first two parameters are the pixel width and pixel height.  Change these
-# to the right size for your display!
-disp = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
+i2c = board.I2C()
+# Define the Reset Pin
+oled_reset = digitalio.DigitalInOut(board.D4)
+disp = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c, addr=0x3C, reset=oled_reset)
 
 # Clear display.
 disp.fill(0)
@@ -63,7 +64,8 @@ def showString(msg):
     disp.show()
 
 def showStatus(msg):
-    # draw.rectangle((0, top + 25, width, 6), outline=0, fill=0)
+    print("showing status: "+msg)
+    draw.rectangle((0, 18, width, height - 1), outline=0, fill=0)
     draw.text((x, top + 25), msg, font=font, fill=255)
     disp.image(image)
     disp.show()
@@ -161,7 +163,8 @@ def statusThread():
             status = subprocess.check_output(app.sta_cmd, shell=True).decode("utf-8")
             app.status = status
             print(app.name+' status: '+status)
-            time.sleep(15)
+            updateAppStatus()
+            time.sleep(10)
         except:
             print("status refresh fails!")
         onStatusTask = False
